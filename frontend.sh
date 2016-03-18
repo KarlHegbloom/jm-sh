81680d463135540035402477d4b942d40490af91
fbennett <biercenator@gmail.com>
2016-03-18 22:00:29 +0900

    Refix paths

--- frontend.sh
+++ frontend.sh
@@ -1,26 +1,26 @@
 # Error handlers
-. jm-lib/errors.sh
+. jm-sh/errors.sh
 
 # Setup
-. jm-lib/setup.sh
+. jm-sh/setup.sh
 
 # Version levels
-. jm-lib/versions.sh
+. jm-sh/versions.sh
 
 # Prompt for options
-#. jm-lib/prompt.sh
+#. jm-sh/prompt.sh
 
 # Parse command-line options
-. jm-lib/opts.sh
+. jm-sh/opts.sh
 
 # Functions for build
-. jm-lib/builder.sh
+. jm-sh/builder.sh
 
 # Functions for release
-. jm-lib/releases.sh
+. jm-sh/releases.sh
 
 # Functions for repo management
-. jm-lib/repo.sh
+. jm-sh/repo.sh
 
 if [ $RELEASE -gt 1 ]; then
   DOORKEY=$(cat "${HOME}/bin/doorkey.txt")
@@ -96,101 +96,4 @@ case $RELEASE in
         repo-finish 0 "Released as FINAL (uploaded to GitHub, full wax)"
         ;;
 esac
-# Error handlers
-. sh-lib/errors.sh
 
-# Setup
-. sh-lib/setup.sh
-
-# Version levels
-. sh-lib/versions.sh
-
-# Prompt for options
-#. sh-lib/prompt.sh
-
-# Parse command-line options
-. sh-lib/opts.sh
-
-# Functions for build
-. sh-lib/builder.sh
-
-# Functions for release
-. sh-lib/releases.sh
-
-# Functions for repo management
-. sh-lib/repo.sh
-
-if [ $RELEASE -gt 1 ]; then
-  DOORKEY=$(cat "${HOME}/bin/doorkey.txt")
-fi
-
-# Perform release ops
-case $RELEASE in
-    1)
-        echo "(1)"
-        # Preliminaries
-        increment-patch-level
-        if [ "$BETA" -gt 0 ]; then
-            increment-beta-level
-        fi
-        echo "Version: ${VERSION}"
-
-        # Build
-        echo "(a)"
-        touch-log
-        echo "(b)"
-        refresh-style-modules
-        echo "(c)"
-        build-the-plugin
-        echo "(d)"
-        repo-finish 1 "Built as ALPHA (no upload to GitHub)"
-        echo "(e)"
-        ;;
-    2)
-        echo "(2)"
-        # Claxon
-        check-for-uncommitted
-        # Preliminaries
-        increment-patch-level
-        increment-beta-level
-        save-beta-level
-        echo "Version is: $VERSION"
-        # Build
-        touch-log
-        refresh-style-modules
-        build-the-plugin
-        git-checkin-all-and-push
-        create-github-release
-        add-xpi-to-github-release
-        repo-finish 0 "Released as BETA (uploaded to GitHub, prerelease)"
-        ;;
-    3)
-        echo "(3)"
-        # Claxon
-        check-for-uncommitted
-        block-uncommitted
-        # Preliminaries
-        reset-beta-level
-        increment-patch-level
-        check-for-release-dir
-        save-patch-level
-        echo "Version is: $VERSION"
-        # Build
-        echo "A"
-        touch-log
-        echo "B"
-        refresh-style-modules
-        echo "C"
-        build-the-plugin
-        echo "D"
-        git-checkin-all-and-push
-        echo "E"
-        create-github-release
-        echo "F"
-        add-xpi-to-github-release
-        echo "G"
-        publish-update
-        echo "H"
-        repo-finish 0 "Released as FINAL (uploaded to GitHub, full wax)"
-        ;;
-esac
