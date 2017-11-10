c1836418e7f290b1ca83dc28098ee37d3bd6c9dc
Karl Hegbloom <karl.hegbloom@gmail.com>
2017-11-10 16:23:42 -0700

    Fix build.sh set-install-version

--- builder.sh
+++ builder.sh
@@ -1,8 +1,4 @@
 function set-install-version () {
-    cat install.rdf | $GSED -e "s,<em:version>.*</em:version>,<em:version>${VERSION}</em:version>," > frag.txt
-    mv frag.txt install.rdf
-    cat install.rdf | $GSED -e "s,<em:updateURL>.*</em:updateURL>,<em:updateURL>https://raw.githubusercontent.com/${GITHUBUSER}/${FORK}/${BRANCH}/update.rdf</em:updateURL>," > frag.txt
-    mv frag.txt install.rdf
-    cat install.rdf | $GSED -e "s,<em:updateURL>.*</em:updateURL>,<em:updateURL>https://raw.githubusercontent.com/${GITHUBUSER}}/${FORK}/${BRANCH}/update.rdf</em:updateURL>," > frag.txt
-    mv frag.txt install.rdf
+    $GSED -ie "s,<em:version>.*</em:version>,<em:version>${VERSION}</em:version>," install.rdf
+    $GSED -ie "s,<em:updateURL>.*</em:updateURL>,<em:updateURL>https://raw.githubusercontent.com/${GITHUBUSER}/${FORK}/${BRANCH}/update.rdf</em:updateURL>,"install.rdf
 }
